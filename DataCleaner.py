3273ee162576a521f1114bbe8878dfc12a865d61
Cojense4 <cojense4@gmail.com>
2024-05-02 16:30:49 -0500

    V0.6.5
    prep_data() is done for now, moved on to gather_data and have it functioning but not that smart, will work on making the function more intelligent next week

--- DataCleaner.py
+++ DataCleaner.py
@@ -90,6 +90,7 @@ def prep_data():
             shutil.rmtree(dir_dict['data'])
             # Copy the data from the import directory to the export directory
             shutil.copytree(import_dir, dir_dict['data'])
+
     user_sensors = []
     sensor_list = [x for x in dir_dict['data'].iterdir() if x.is_dir()]
     for sensor in sensor_list:
@@ -102,129 +103,60 @@ def prep_data():
                 for file in new_dir.iterdir():
                     name, ext = file.stem, file.suffix
                     name = name.split('_')[-1]
-                    new_file_name = f"{sensor_type}_{name}{ext}"
+                    new_file_name = f"{sensor_type[0]}_{name}{ext}"
                     file.rename(new_dir / new_file_name)
-                    create_directory(dir_dict['gather']/ sensor_type / name)
                 break
         else:
-            sys.exit(f'Warning: ({sensor.name}) not recognized')
+            print(f'Warning: ({sensor.name}) not recognized or already created')
 
     return dir_dict, user_sensors
 
 
 def gather_data(dir_dict):
-
-    """
-    Gather and process data from different sensors.
-
-    Args:
-        dir_dict (dict): A dictionary containing the data sensor path.
-
-    Returns:
-        None
-
-    Raises:
-        SystemExit: If a sensor is not recognized.
-
-    """
     def create_directory(path):
-        """Recursively creates a directory if it does not exist."""
         if not os.path.exists(path):
-            try:
-                os.makedirs(path)
-            except OSError as e:
-                if len(path.split(os.sep)) > 2:
-                    response = input(f"The path '{path}' does not exist. Do you want to create it? [(0)No/(1)Yes]: ")
-                    if response == '1':
-                        create_directory(os.path.dirname(path))
-                        create_directory(path)
-                    else:
-                        sys.exit('Directory creation cancelled.')
-                else:
-                    sys.exit(f"Failed to create directory: {e}")
+            os.makedirs(path, exist_ok=True)
 
-    def count_header_rows(file_path):
-        with open(file_path, 'r') as f:
-            header_rows = 0
-            for line in f:
-                header_rows += 1
-                if '#DATA' in line:
-                    header_rows += 1
-                    break
-        return header_rows
-
-    def process_body(file_path, header_rows, column_headers):
-        df_body = pd.read_csv(file_path, skiprows=header_rows, header=0, low_memory=False)
-
-        # Ask the user to select which columns to keep
-        columns_to_keep = input("Enter the numbers of the columns to keep, separated by commas (use a dash to specify a range): ")
-
-        # Process the input
-        columns_to_keep = process_input(columns_to_keep)
-        columns_to_keep = [df_body.columns[i-1] for i in columns_to_keep]
-
-        # Drop the unselected columns
-        df_body = df_body[columns_to_keep]
-
-        return df_body
-    
-    def process_header(file_path, columns_to_keep=None):
-        # Load the data...
-        if columns_to_keep is None:
-            # Get a list of column headers
-            column_headers = df_header.columns.tolist()
-
-            # Print the column headers
-            print("Column headers:")
-            for i, header in enumerate(column_headers, 1):
-                print(f"{i}. {header}")
-
-            # Ask the user to select which columns to keep
-            columns_to_keep = input("Enter the numbers of the columns to keep, separated by commas (use a dash to specify a range): ")
-
-            # Process the input
-            columns_to_keep = process_input(columns_to_keep)
-
-        # Drop the unselected columns
-        df_header = df_header[columns_to_keep]
-
-        return df_header, columns_to_keep
-    
-    def process_input(user_input):
-        user_input = user_input.split(",")
-        user_input = [range(int(x.split("-")[0]), int(x.split("-")[1])+1) if "-" in x else int(x) for x in user_input]
-        user_input = [item for sublist in user_input for item in sublist]  # Flatten the list
-        return user_input
-    
     for sensor in os.listdir(dir_dict['data']):
-        columns_to_keep = None
         print(f'Processing {sensor} data...')
         gather_sensor_dir = os.path.join(dir_dict['gather'], sensor)
-        create_directory(gather_sensor_dir)
         sensor_dir = os.path.join(dir_dict['data'], sensor)
+        columns_to_keep = None
+        prev_num_columns = 0
+
         for sensor_file in os.listdir(sensor_dir):
             if sensor_file.endswith('.csv'):
-                    source_file_path = os.path.join(sensor_dir, sensor_file)
-                        
-                    # Determine the number of header rows
-                    header_rows = count_header_rows(source_file_path)
-                        
-                    # Process the header
-                    if columns_to_keep is None:
-                        df_header, columns_to_keep = process_header(source_file_path)
-                    else:
-                        df_header = process_header(source_file_path, columns_to_keep)
-                        
-                    # Process the body
-                    df_body = process_body(source_file_path, header_rows, df_header.columns.tolist())
+                source_file_path = os.path.join(sensor_dir, sensor_file)
+                with open(source_file_path, 'r') as f:
+                    reader = csv.reader(f)
+                    for i, row in enumerate(reader, start=1):
+                        if row and row[0] == '#METADATA':
+                            meta_rows = i-2
+                        elif row and row[0] == '#DATA':
+                            header_rows = i
+                            break
+                df_head = pd.read_csv(source_file_path, nrows=meta_rows)
+                df_head.to_csv(f'{os.path.join(gather_sensor_dir, f'{sensor_file[:-4]}_header.csv')}', index=False, header=True)
+                df_body = pd.read_csv(source_file_path, skiprows=header_rows, header=0, low_memory=False)
 
-                    # Save the processed data
-                    name = sensor_file.split('_')[-1][:-4]
-                    dest_file_path = os.path.join(gather_sensor_dir, name)
-                    create_directory(dest_file_path)    
-                    df_header.to_csv(f'{os.path.join(dest_file_path, 'header.csv')}', index=False, header=False)
-                    df_body.to_csv(f'{os.path.join(dest_file_path, 'body.csv')}', index=False, header=True)
-                    break
+                num_columns = len(df_body.columns)
+
+                if columns_to_keep is None or num_columns != prev_num_columns:
+                    print("Column headers:")
+                    for i, header in enumerate(df_body.columns.tolist(), 1):
+                        print(f"{i}. {header}")
+                        col_num = i
+                    user_input = input("Enter the numbers of the columns to keep, comma seperated, use dash for range: ")
+                    if user_input == '0':
+                        user_input = f'1-{col_num - 1}'
+                    user_input = user_input.split(",")
+                    user_input = [range(int(x.split("-")[0]), int(x.split("-")[1])+1) if "-" in x else int(x) for x in user_input]
+                    columns_to_keep = [item for sublist in user_input for item in sublist]  # Flatten the list
+                    prev_num_columns = num_columns
+
+                df_body = df_body[df_body.columns[columns_to_keep]]
+                df_body.to_csv(f'{os.path.join(gather_sensor_dir, f'{sensor_file[:-4]}_body.csv')}', index=False, header=True)
+                #TODO: Finish this section of the code, should be finding a way to smartly detect which columns to keep based on the data
 
 
 
@@ -242,5 +174,4 @@ def clean_data(dir_dict):
     return None
 
 if __name__ == '__main__':
-    dir_dict, sensor_list = prep_data()
-    print(sensor_list)
+    gather_data({'export': 'C:/Users/cjensen32/Downloads/iMotions_Exports', 'gather': 'C:/Users/cjensen32/Downloads/iMotions_Exports/Gather', 'results': 'C:/Users/cjensen32/Downloads/iMotions_Exports/Results', 'data': 'C:/Users/cjensen32/Downloads/iMotions_Exports/Data'})
